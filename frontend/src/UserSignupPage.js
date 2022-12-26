2dc0ea0ee260558096e3657caee52ca50a9d2ab0
Fatih Metin <fatih1896@gmail.com>
2022-12-26 19:50:15 +0300

    initial commit

---
+++ frontend/src/UserSignupPage.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+class UserSignupPage extends React.Component {
+
+
+    state = {
+        username: null,
+        display: null,
+        password: null,
+        passwordRepeat: null
+    }
+
+    onChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
+
+
+    render() {
+        return (
+            <form>
+                <h1>Sign Up</h1>
+                <div>
+                    <label>Username</label>
+                    <input name="username" onChange={this.onChange} />
+                </div>
+                <div>
+                    <label>Display Name</label>
+                    <input name="display" onChange={this.onChange} />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input name="password" type="password" onChange={this.onChange} />
+                </div>
+                <div>
+                    <label>Password Repeat</label>
+                    <input name="passwordRepeat" type="password" onChange={this.onChange} />
+                </div>
+                <button >Sign Up</button>
+
+            </form>
+        );
+    }
+}
+
+export default UserSignupPage;
