12d700b5f5d277e767de56c828ecc77c4171a2bf
Fatih Metin <fatih1896@gmail.com>
2023-01-04 16:30:30 +0300

    using local storage to save global state

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,20 +1,35 @@
 import { legacy_createStore as createStore } from 'redux'
 import authReducer from './authReducer'
 
-const loggedInState = {
-    isLoggedIn: true,
-    username: 'user1',
-    displayName: 'display1',
-    image: undefined,
-    password: 'P4ssword'
-}
 
 const configureStore = () => {
-    return createStore(
+    const hoaxAuth = localStorage.getItem('hoax-auth');
+    let stateInLocalStorage = {
+        isLoggedIn: false,
+        username: undefined,
+        displayName: undefined,
+        image: undefined,
+        password: undefined
+    }
+
+    if (hoaxAuth) {
+        try {
+            stateInLocalStorage = JSON.parse(hoaxAuth);
+        } catch (error) {
+            
+        }
+    }
+
+    const store = createStore(
         authReducer,
-        loggedInState,
+        stateInLocalStorage,
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
+
+    store.subscribe(() => {
+        localStorage.setItem('hoax-auth', JSON.stringify(store.getState()));
+    })
+    return store;
 }
 
 export default configureStore;
