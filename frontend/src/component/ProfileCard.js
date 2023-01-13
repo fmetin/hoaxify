07029f307984e2a796e0d73fa6c54c83e2293aaa
Fatih Metin <fatih1896@gmail.com>
2023-01-13 15:59:27 +0300

    interaction with form in profile card

--- frontend/src/component/ProfileCard.js
+++ frontend/src/component/ProfileCard.js
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import Input from './Input';
 import ProfileImageWithDefault from './ProfileImageWithDefault';
 const ProfileCard = (props) => {
-
     const [inEditMode, setInEditMode] = useState(false);
+    const [updatedDisplayName, setUpdatedDisplayName] = useState();
+
+    const onClickSave = () => {
+        console.log(updatedDisplayName);
+    }
 
     const { user } = props;
     const { username, displayName, image } = user;
@@ -15,6 +19,14 @@ const ProfileCard = (props) => {
     const { username: loggedInUsername } = useSelector(store => ({ username: store.username }));
     const routeParams = useParams();
     const pathUserName = routeParams.username;
+
+    useEffect(() => {
+        if (inEditMode) {
+            setUpdatedDisplayName(displayName);
+        } else {
+            setUpdatedDisplayName(undefined);
+        }
+    }, [inEditMode, displayName])
 
     return (
         <div className="card text-center">
@@ -44,9 +56,9 @@ const ProfileCard = (props) => {
                 )}
                 {inEditMode &&
                     <div>
-                        <Input label={t('change.display.name')} ></Input>
+                        <Input label={t('change.display.name')} defaultValue={displayName} onChange={(event) => { setUpdatedDisplayName(event.target.value) }} ></Input>
                         <div>
-                            <button className="btn btn-primary d-inline-flex">
+                            <button className="btn btn-primary d-inline-flex" onClick={onClickSave}>
                                 <i className="material-icons">save</i>
                                 {t('save')}
                             </button>
